265a44a3bf257c992fb8cd85cb2d1856c14cafb3
jae <j.kerr.clayton@gmail.com>
2022-06-06 17:52:01 -0500

    refactoring of first funciton // creating new data set for letter_bank

--- src/adagrams.js
+++ src/adagrams.js
@@ -28,6 +28,31 @@ const LETTER_POOL = {
 };
 
 
+const letter = Object.keys(LETTER_POOL); 
+const vals = Object.keys(LETTER_POOL).map(key => LETTER_POOL[key]);
+const letter_list = [] 
+let i = 0 
+for (i = 0; i < letter.length; i++){
+  let count = 0 
+  while (count != vals[i]){
+    letter_list.push(letter[i])
+    count += 1}
+  // i += 1; 
+  
+  }
+
+export const drawLetters = () => {
+  const set = new Set();
+  while (set.size !== 10) {
+    set.add(Math.floor(Math.random() * letter_list.length));
+  }
+  const numbers = [...set];
+  let hand = [] 
+  for (const num of numbers){
+    hand.push(letter_list[num])
+  }return hand;  
+};
+    
 
 
 const letterScore = {
@@ -61,31 +86,26 @@ const letterScore = {
   
 
 
-// If the length of the word is 7, 8, 9, or 10, 
-// then the word gets an additional 8 points
-
-
-
 // CONSIDER CHANGING DATA STRUCTURE TO REFLECT ## OF LETTERS (PROBABILITY!)
 // for letterCopy 
-export const drawLetters = () => {
-  let letterBank = []
-  let letterCopy = Object.assign({}, LETTER_POOL)
-  let i = 0 
-  let keys = Object.keys(letterCopy)
-  // let prop = keys[Math.floor(Math.random() * keys.length)]
-  while (i < 10) {
-    let prop = keys[Math.floor(Math.random() * keys.length)]
-    if (letterCopy[prop] > 0){
-      letterBank[i] = prop;
-      letterCopy[prop] -= 1;
+// export const drawLetters = () => {
+//   let letterBank = []
+//   let letterCopy = Object.assign({}, LETTER_POOL)
+//   let i = 0 
+//   let keys = Object.keys(letterCopy)
+//   // let prop = keys[Math.floor(Math.random() * keys.length)]
+//   while (i < 10) {
+//     let prop = keys[Math.floor(Math.random() * keys.length)]
+//     if (letterCopy[prop] > 0){
+//       letterBank[i] = prop;
+//       letterCopy[prop] -= 1;
     
-      i ++;
-      }
-    }
-    // console.log(letterBank);
-    return letterBank;
-};
+//       i ++;
+//       }
+//     }
+//     // console.log(letterBank);
+//     return letterBank;
+// };
   
 // console.log(letterBank());
   
@@ -113,21 +133,9 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 
 }; 
 
-// letterScore
-// Letter	Value
-// [A, E, I, O, U, L, N, R, S, T]	1
-// [D, G]	2
-// [B, C, M, P]	3
-// [F, H, V, W, Y]	4
-// [K]	5
-// [J, X]	8
-// [Q, Z] 	10
-
-// If the length of the word is 7, 8, 9, or 10, 
-// then the word gets an additional 8 points
 
 export const wordInfo = (word) => {
-  let wordInfoDict = {}
+  let wordInfoObj = {}
   if (word.length == 0){
     return 0;
   }
@@ -141,12 +149,11 @@ export const wordInfo = (word) => {
   }
   for (const key in wordHash) {
     counter += wordHash[key];
-  }wordInfoDict['word'] = word;
-  wordInfoDict['score'] = counter; 
-  return wordInfoDict; 
+  }wordInfoObj['word'] = word;
+  wordInfoObj['score'] = counter; 
+  return wordInfoObj; 
  
 };
-
 
 export const scoreWord = (word) => {
   if (word.length == 0){
@@ -155,28 +162,6 @@ export const scoreWord = (word) => {
   let results = wordInfo(word); 
   return results['score']; 
 };
-  // wordInfo(word); 
-
-//   if (word.length == 0){
-//     return 0;
-//   }
-//   const input = word.toUpperCase()
-//   let wordHash = {};
-//   let counter = word.length > 6 ? 8 : 0;
-                  
-//   for (const letter of input){
-
-//     wordHash[letter] = wordHash[letter]? (wordHash[letter] + letterScore[letter])  : letterScore[letter];
-//   }
-//   for (const key in wordHash) {
-//     counter += wordHash[key];
-//   }
-//   return counter; 
-
-// };
-
-// it("returns a hash that contains the word and score of best word in an array", () => {
-//   const words = ["X", "XX", "XXX", "XXXX"];
 
 
 export const highestScoreFrom = (words) => {
@@ -191,6 +176,8 @@ export const highestScoreFrom = (words) => {
       let amnt = scoreWord(word);
       scores.push(amnt)
     }
+
+    // find max score & sort words by length 
   const winner = Math.max(...scores);
   const word_sort = words.sort((a,b) => a.length - b.length);
   
@@ -198,61 +185,7 @@ export const highestScoreFrom = (words) => {
     let word_dict = wordInfo(word); 
     if (word_dict.score == winner){
       allBest.push(word_dict)};
-  }
-  // if (allBest.length > 1) { 
-  //   allBest.sort((a,b) => a.length - b.length);
-  // }
-  // let min = Math.min(...allBest.map(({ length }) => length));
-  // let shortest_word = Math.min(...allBest)
-  // console.log(allBest[0]);
-  // console.log('ONE WORD'); 
-  return allBest[0]; 
-  
-
+  } return allBest[0]; 
   
 };
     
-// export const highestScoreFrom = (words) => {
-//     let winningScore = {}
-//     let allInfo = []
-//     let scores = []
-//     for (const word of words){
-//       if(word.length == 10){
-//         return wordInfo(word);
-//       }
-//       allInfo.push(wordInfo(word)); 
-//       let amnt = scoreWord(word);
-//       scores.push(amnt)
-//       }const winner = Math.max(...scores);
-//       const index = scores.indexOf(winner);
-//      for (const word of words)
-//       console.log(words[index], winner);
-//       let winning_word = words[index];
-   
-//       winningScore['word'] = winning_word; 
-//       winningScore['score'] = winner; 
-//       console.log(winningScore);
-//       if (winningScore.length > 1){
-//         return winningScore[0];
-//       }
-//       return winningScore;
-
-//     };
-
-
-// export const highestScoreFrom = (words) => {
-//   let winningScore = {}
-//   let scores = []
-//   for (const word of words){
-//     let amnt = scoreWord(word);
-//     scores.push(amnt)
-//     }const winner = Math.max(...scores);
-//     const index = scores.indexOf(winner);
-//     console.log(words[index], winner);
-//     let winning_word = words[index];
-  
-//     winningScore['word'] = winning_word; 
-//     winningScore['score'] = winner; 
-//     return winningScore;
-
-//   };
