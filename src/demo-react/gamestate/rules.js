a7e98240893536c6f307d181ed2c5c7c49436286
Matt McKnett <mmcknett@gmail.com>
2022-06-09 01:57:17 -0700

    Convert guesses to uppercase
    so that users aren't required to caps lock

--- src/demo-react/gamestate/rules.js
+++ src/demo-react/gamestate/rules.js
@@ -6,15 +6,24 @@ import Adagrams from 'demo/adagrams';
 export function validateGuessInput(wrappedReducer) {
   return (state, action) => {
     if (action.type === Actions.GUESS) {
-      const word = action.payload;
+      const word = action.payload.toUpperCase();
+
       if (wordHasBeenGuessed(state, word)) {
         const errAction = new SetErrorAction(`${word} was already guessed!`);
         return wrappedReducer(state, errAction);
       }
+
+      if (!word) {
+        return wrappedReducer(state, new SetErrorAction('Enter a word!'));
+      }
+
       if (!Adagrams.usesAvailableLetters(word, state.currentHand)) {
         const errAction = new SetErrorAction(`${word} isn't valid!`);
         return wrappedReducer(state, errAction);
       }
+
+      // Ensure that the word is uppercased
+      return wrappedReducer(state, { ...action, payload: word });
     }
 
     return wrappedReducer(state, action);
