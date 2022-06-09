eb88989570f2d83bfa8ea4cba3ed6136b4a3de47
Matt McKnett <mmcknett@gmail.com>
2022-06-09 01:58:58 -0700

    Fill out the win screen
    This implementation shows who won and shows the total score for each
    player. The only missing behavior is showing the highest-scoring word
    each player guessed.

--- src/demo-react/screens/win.js
+++ src/demo-react/screens/win.js
@@ -1,17 +1,35 @@
 import React from 'react';
 
-import { Box, Text } from 'ink';
+import { Box, Newline, Text, useApp } from 'ink';
 
 import * as Actions from '../gamestate/action-types';
 import { Menu, MenuEntry } from '../components/menu';
 import { useGameStateContext } from '../components/gamestate-context';
+import Adagrams from 'demo/adagrams';
 
 export default function Win() {
   const { state, dispatch } = useGameStateContext();
+  const { exit } = useApp();
   const menu = [
     MenuEntry('Rematch!', () => dispatch({ type: Actions.REMATCH })),
-    MenuEntry('Start Over', () => dispatch({ type: Actions.RESET }), 'red')
+    MenuEntry('Start Over', () => dispatch({ type: Actions.RESET })),
+    MenuEntry('Quit', exit, 'red')
   ];
+
+  const playerScores = state.players.map(
+    player => { 
+      const flattenedWords = [].concat(...player.words);
+      const scores = flattenedWords.map(Adagrams.scoreWord);
+      const totalScore = scores.reduce((sum, n) => sum + n, 0);
+      return {
+        name: player.name,
+        score: totalScore
+      };
+    }
+  );
+  playerScores.sort((a, b) => b.score - a.score);
+  const winningScore = playerScores[0].score;
+  const winner = playerScores[0].name;
 
   return (
     <Box
@@ -20,7 +38,18 @@ export default function Win() {
       borderStyle='round'
       padding='1'
     >
-      <Text color='yellow'>TODO: Make a win screen!</Text>
+      <Text>
+        <Text color='blueBright'>{ winner } </Text>
+        wins with a score of&nbsp;
+        <Text color='blueBright'>{ winningScore }</Text>!
+      </Text>
+      <Newline />
+      {
+        playerScores.map(p => 
+          <Text key={ p.name } color='gray'>{ p.name }: { p.score }</Text>
+        )
+      }
+      <Newline />
       <Menu items={ menu } />
     </Box>
   );
