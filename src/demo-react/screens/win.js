4b40423d0b7c869f563a9f42737e72327d0cd82a
Matt McKnett <mmcknett@gmail.com>
2022-06-10 21:46:17 -0700

    Show best word stats and reorganize Win screen

--- src/demo-react/screens/win.js
+++ src/demo-react/screens/win.js
@@ -19,11 +19,13 @@ export default function Win() {
   const playerScores = state.players.map(
     player => { 
       const flattenedWords = [].concat(...player.words);
-      const scores = flattenedWords.map(Adagrams.scoreWord);
-      const totalScore = scores.reduce((sum, n) => sum + n, 0);
+      const scores = flattenedWords.map(word => ({ word, score: Adagrams.scoreWord(word)}));
+      scores.sort((a, b) => b.score - a.score); // Sort by highest scoring word.
+      const totalScore = scores.reduce((sum, scoreAndWord) => sum + scoreAndWord.score, 0);
       return {
         name: player.name,
-        score: totalScore
+        score: totalScore,
+        bestWord: scores.length > 0 && `${scores[0].word} (${scores[0].score})` || ''
       };
     }
   );
@@ -38,17 +40,48 @@ export default function Win() {
       borderStyle='round'
       padding='1'
     >
-      <Text>
-        <Text color='blueBright'>{ winner } </Text>
-        wins with a score of&nbsp;
-        <Text color='blueBright'>{ winningScore }</Text>!
-      </Text>
-      <Newline />
-      {
-        playerScores.map(p => 
-          <Text key={ p.name } color='gray'>{ p.name }: { p.score }</Text>
-        )
-      }
+      <Box
+        borderColor='yellowBright'
+        borderStyle='round'
+        paddingY='1'
+        paddingX='3'
+        margin='1'
+      >
+        <Text>
+          <Text color='yellowBright'>{ winner } </Text>
+          wins with a score of&nbsp;
+          <Text color='yellowBright'>{ winningScore }</Text>!
+        </Text>
+      </Box>
+      <Box
+        width='100%'
+        marginX='2'
+        flexDirection='row'
+        justifyContent='space-around'
+      >
+        <Box
+          flexDirection='column'
+        >
+          <Box marginBottom='1'><Text color='blueBright'>Best words:</Text></Box>
+          {
+            playerScores.map(p => <Text key={ p.name } color='gray'>{ p.name }: <Text color='blueBright'>{ p.bestWord }</Text></Text>)
+          }
+        </Box>
+        <Box
+          flexDirection='column'
+        >
+          <Box marginBottom='1'><Text color='blueBright'>Leaderboard:</Text></Box>
+          {
+            playerScores.map(p => <Text key={ p.name } color='gray'>{ p.name }: <Text color='blueBright'>{ p.score }</Text></Text>)
+          }
+        </Box>
+        <Box
+          flexDirection='column'
+        >
+          <Text color='blueBright'>TODO: Round scores go here</Text>
+        </Box>
+      </Box>
+     
       <Newline />
       <Menu items={ menu } />
     </Box>
