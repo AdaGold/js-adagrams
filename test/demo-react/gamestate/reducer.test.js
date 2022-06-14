9b8aaa1537f5897f55ce464afa79d7ac82407450
Matt McKnett <mmcknett@gmail.com>
2022-06-13 21:50:43 -0700

    test: Add tests for error state and bounds checks

--- test/demo-react/gamestate/reducer.test.js
+++ test/demo-react/gamestate/reducer.test.js
@@ -17,6 +17,14 @@ function assertSimpleActionUpdatesKey(actionType, keyName, value = 'any-value') 
   expect(actual).toEqual(expected);
 }
 
+const mockPlayerList = [
+  { name: 'Max', words: [['HELO', 'LOW'], ['WORLD']]},
+  { name: 'Min Soo', words: [[], ['OLE']]},
+  { name: 'Rupa', words: [['HELLO'], ['DROLL', 'ROLE']]},
+  { name: 'Elliot', words: [['DROOL'], ['WHORL']]}
+]
+
+
 describe('Game state reducer', () => {
   describe('Simple actions', () => {
     test('Switch screen', () => {
@@ -24,17 +32,97 @@ describe('Game state reducer', () => {
         Actions.SWITCH_SCREEN, 'currentScreen');
     });
 
-    test('Set number of rounds', () => {
+    test('Reset returns to the initial state', () => {
+      const action = makeAction(Actions.RESET);
+      const currentState = {
+        ...initialState,
+        currentScreen: ScreenId.WIN,
+        currentRound: 3,
+        currentPlayer: 3,
+        players: mockPlayerList
+      };
+
+      const actual = reducer(currentState, action);
+
+      expect(actual).toEqual(initialState);
+    })
+  });
+
+  describe('Set number of rounds', () => {
+    test('Set 1 round, succeeds', () => {
       assertSimpleActionUpdatesKey(
         Actions.SET_NUMBER_ROUNDS, 'roundsPerGame', 1);
     });
 
-    test('Set desired number of players', () => {
+    test('Set 5 rounds, succeeds', () => {
+      assertSimpleActionUpdatesKey(
+        Actions.SET_NUMBER_ROUNDS, 'roundsPerGame', 5);
+    });
+    
+    test('Set 0 rounds, sets error', () => {
+      const action = makeAction(Actions.SET_NUMBER_ROUNDS, 0);
+      const actual = reducer(initialState, action);
+      expect(actual).toEqual({
+        ...initialState,
+        lastError: '0 is not a valid number of rounds.'
+      });
+    });
+
+    test('Set 6 rounds, sets error', () => {
+      const action = makeAction(Actions.SET_NUMBER_ROUNDS, 6);
+      const actual = reducer(initialState, action);
+      expect(actual).toEqual({
+        ...initialState,
+        lastError: '6 is not a valid number of rounds.'
+      });
+    });
+  });
+
+  describe('Set desired number of players', () => {
+    test('Set desired number of players to 1, succeeds', () => {
+      assertSimpleActionUpdatesKey(
+        Actions.SET_DESIRED_PLAYERS, 'desiredPlayers', 1);
+    });
+
+    test('Set desired number of players to 4, succeeds', () => {
       assertSimpleActionUpdatesKey(
         Actions.SET_DESIRED_PLAYERS, 'desiredPlayers', 4);
     });
 
-    test('Set number of seconds per turn', () => {
+    test('Set desired number of players to 0, sets error', () => {
+      const action = makeAction(Actions.SET_DESIRED_PLAYERS, 0);
+      const actual = reducer(initialState, action);
+      expect(actual).toEqual({
+        ...initialState,
+        lastError: '0 is not a valid number of players.'
+      });
+    });
+
+    test('Set desired number of players to 5, sets error', () => {
+      const action = makeAction(Actions.SET_DESIRED_PLAYERS, 5);
+      const actual = reducer(initialState, action);
+      expect(actual).toEqual({
+        ...initialState,
+        lastError: '5 is not a valid number of players.'
+      });
+    });
+  });
+
+  describe('Set number of seconds per turn', () => {
+    test('Set 10 seconds per turn, succeeds', () => {
+      const action = makeAction(Actions.SET_TURN_SECONDS, 10);
+      const expected = {
+        ...initialState,
+        gameTimer: 10,
+        secondsPerTurn: 10
+      }
+
+      const actual = reducer(initialState, action);
+
+      expect(actual).toEqual(expected);
+    });
+
+    test('Set 60 seconds per turn, succeeds', () => {
       const action = makeAction(Actions.SET_TURN_SECONDS, 60);
       const expected = {
         ...initialState,
@@ -47,32 +135,87 @@ describe('Game state reducer', () => {
       expect(actual).toEqual(expected);
     });
 
-    test('Reset returns to the initial state', () => {
-      const action = makeAction(Actions.RESET);
-      const currentState = {
+    test('Set 9 seconds per turn, sets error', () => {
+      const action = makeAction(Actions.SET_TURN_SECONDS, 9);
+      const expected = {
         ...initialState,
-        currentScreen: ScreenId.WIN
-      };
+        lastError: "9 is not a valid number of seconds for each player's turn."
+      }
 
-      const actual = reducer(currentState, action);
+      const actual = reducer(initialState, action);
 
-      expect(actual).toEqual(initialState);
-    })
+      expect(actual).toEqual(expected);
+    });
+
+    test('Set 61 seconds per turn, sets error', () => {
+      const action = makeAction(Actions.SET_TURN_SECONDS, 61);
+      const expected = {
+        ...initialState,
+        lastError: "61 is not a valid number of seconds for each player's turn."
+      }
+
+      const actual = reducer(initialState, action);
+
+      expect(actual).toEqual(expected);
+    });
   });
 
   describe('Add Player', () => {
-    // TODO...
-    // Test: Add a new player, succeeds
-    // Test: Add a player with a blank name, sets error
-    // Test: Add a player with the same name as another, sets error
-  })
+    test('Add player adds the player and a guess list for the first round', () => {
+      const expectedState = {
+        ...initialState,
+        players: [{ name: 'Player One', words: [[]] }]
+      };
+      const addPlayerOne = makeAction(Actions.ADD_PLAYER, 'Player One');
+
+      const actual = reducer(initialState, addPlayerOne);
+
+      expect(actual).toEqual(expectedState)
+    });
+
+    test('Add player with no name, sets error', () => {
+      const expectedState = {
+        ...initialState,
+        lastError: 'Enter a name!'
+      };
+      const addPlayerOne = makeAction(Actions.ADD_PLAYER, '');
+
+      const actual = reducer(initialState, addPlayerOne);
+
+      expect(actual).toEqual(expectedState)
+    });
+
+    test('Add player with existing player\'s name, sets error', () => {
+      const expectedState = {
+        ...initialState,
+        players: [{ name: 'Player One', words: [[]] }],
+        lastError: 'A player named Player One already exists!'
+      };
+      const addPlayerOne = makeAction(Actions.ADD_PLAYER, 'Player One');
+
+      let actual = reducer(initialState, addPlayerOne);
+      actual = reducer(actual, addPlayerOne);
+
+      expect(actual).toEqual(expectedState)
+    });
+  });
 
   describe('Error handling', () => {
     test('Set last error', () => {
       assertSimpleActionUpdatesKey(Actions.SET_ERROR, 'lastError');
     });
 
-    // TODO...
-    // Test: Error is cleared after any other action.
+    test('Any action clears last error', () => {
+      const currentState = { ...initialState, lastError: 'Some error' };
+      const expected = { ...currentState, lastError: '' };
+      const action = makeAction(
+        'literally-anything-besides-set-error',
+        'the-payload' 
+      );
+
+      const actual = reducer(currentState, action);
+
+      expect(actual).toEqual(expected);
+    });
   });
 });
