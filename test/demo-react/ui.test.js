16a423c136093af5ae5edbb8a3acce94385c3674
Matt McKnett <mmcknett@gmail.com>
2022-07-12 14:31:58 -0700

    test: update basic ui tests

--- test/demo-react/ui.test.js
+++ test/demo-react/ui.test.js
@@ -3,16 +3,59 @@ import App from 'demo-react/ui';
 import { render } from 'ink-testing-library';
 import { expectRenderToMatchSnapshot } from './expect-render';
 
-describe('Button rendering', () => {
+const rightArrow = (renderResult, jsx) => {
+  const { stdin, rerender } = renderResult;
+  stdin.write('\u001B[C');
+  rerender(jsx);  // Hack: Force React/Ink to run useEffect handlers so the useInput callback gets registered.
+}
+const enter = stdin => stdin.write('\r');
+
+describe('Adagrams App', () => {
   it('Renders correctly in the default state', () => {
     expectRenderToMatchSnapshot(<App />);
   });
 
-  it.skip('Highlights the next item on right-arrow press', () => {
-    const { stdin, lastFrame } = render(<App />);
+  it('Highlights the next item on right-arrow press', () => {
+    const jsx = <App />;
+    const rr = render(jsx);
+    rr.rerender(jsx); // Hack: Force React/Ink to run useEffect handlers so the useInput callback gets registered.
 
-    stdin.write(String.fromCharCode(13) + String.fromCharCode(10)); // Try to send [Enter]
+    rightArrow(rr, jsx);
 
-    expect(lastFrame()).toMatchSnapshot();
+    expect(rr.lastFrame()).toMatchSnapshot();
+  });
+
+  it('Highlights quit on two right-arrow presses', () => {
+    const jsx = <App />;
+    const rr = render(jsx);
+    rr.rerender(jsx); // Hack: Force React/Ink to run useEffect handlers so the useInput callback gets registered.
+
+    rightArrow(rr, jsx);
+    rightArrow(rr, jsx);
+
+    expect(rr.lastFrame()).toMatchSnapshot();
+  });
+
+  it('Quits if quit is selected', () => {
+    const jsx = <App />;
+    const rr = render(jsx);
+    rr.rerender(jsx); // Hack: Force React/Ink to run useEffect handlers so the useInput callback gets registered.
+
+    rightArrow(rr, jsx);
+    rightArrow(rr, jsx);
+    enter(rr.stdin);
+
+    expect(rr.lastFrame()).toMatchSnapshot();
+  });
+
+  it('Shows the help screen', () => {
+    const jsx = <App />;
+    const rr = render(jsx);
+    rr.rerender(jsx); // Hack: Force React/Ink to run useEffect handlers so the useInput callback gets registered.
+
+    rightArrow(rr, jsx);
+    enter(rr.stdin);
+
+    expect(rr.lastFrame()).toMatchSnapshot();
   });
 });
