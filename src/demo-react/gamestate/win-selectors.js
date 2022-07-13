1b16d4a4128ed06cb3310fdcf48cd223ddba1102
Matt McKnett <mmcknett@gmail.com>
2022-07-12 17:53:07 -0700

    fix: Use highestScoreFrom in WinScreenInfo

--- src/demo-react/gamestate/win-selectors.js
+++ src/demo-react/gamestate/win-selectors.js
@@ -5,13 +5,13 @@ export class WinScreenInfo {
     this.playerScores = state.players.map(
       player => { 
         const flattenedWords = [].concat(...player.words);
-        const scores = flattenedWords.map(word => ({ word, score: Adagrams.scoreWord(word)}));
-        scores.sort((a, b) => b.score - a.score); // Sort by highest scoring word.
-        const totalScore = scores.reduce((sum, scoreAndWord) => sum + scoreAndWord.score, 0);
+        const scores = flattenedWords.map(Adagrams.scoreWord);
+        const totalScore = scores.reduce((sum, score) => sum + score, 0);
+        const bestWord = Adagrams.highestScoreFrom(flattenedWords);
         return {
           name: player.name,
           score: totalScore,
-          bestWord: scores.length > 0 && `${scores[0].word} (${scores[0].score})` || ''
+          bestWord: bestWord && `${bestWord.word} (${bestWord.score})` || ''
         };
       }
     );
@@ -20,7 +20,7 @@ export class WinScreenInfo {
     this.roundWinners = new Array(state.players[0].words.length);
     for (let i = 0; i < this.roundWinners.length; ++i) {
       const playerScoresThisRound = state.players.map(player => {
-        const scores = player.words[i].map(word => Adagrams.scoreWord(word));
+        const scores = player.words[i].map(Adagrams.scoreWord);
         const roundScore = scores.reduce((sum, score) => sum + score, 0);
         return {
           score: roundScore,
