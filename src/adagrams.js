cfe1b8e1f59a4da2cd7c70d692cf37517eb1250c
Tasha <tashachang2@gmail.com>
2022-11-29 21:07:35 -0800

    Completed wave 1 drawLetters function

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,5 +1,48 @@
+const letterPool = {
+  A: 9,
+  B: 2,
+  C: 2,
+  D: 4,
+  E: 12,
+  F: 2,
+  G: 3,
+  H: 2,
+  I: 9,
+  J: 1,
+  K: 1,
+  L: 4,
+  M: 2,
+  N: 6,
+  O: 8,
+  P: 2,
+  Q: 1,
+  R: 6,
+  S: 4,
+  T: 6,
+  U: 4,
+  V: 2,
+  W: 2,
+  X: 1,
+  Y: 2,
+  Z: 1,
+};
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
 export const drawLetters = () => {
   // Implement this method for wave 1
+  let letterPoolDict = letterPool;
+  let hand = [];
+  let i = 0;
+
+  while (i < 10) {
+    let letter = alphabet[Math.floor(Math.random() * alphabet.length)];
+    if (letterPoolDict[letter] < 0) {
+      letterPoolDict[letter] -= 1;
+      hand.push(letter);
+      i++;
+    }
+  }
+  return hand;
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
