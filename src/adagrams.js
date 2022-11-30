591024ab879bd08cc766e70797cd32618b5b1ebd
Dominique <dominiquedaba@gmail.com>
2022-11-30 14:52:41 -0800

    drawLetters implementation passes all tests

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,5 +1,64 @@
+const LETTER_POOL = {
+  A: 9,
+  B: 2,
+  C: 2,
+  D: 4,
+  E: 12,
+  F: 2,
+  G: 3,
+  H: 2,
+  I: 9,
+  J: 1,
+  K: 1,
+  L: 4,
+  M: 2,
+  N: 6,
+  O: 8,
+  P: 2,
+  Q: 1,
+  R: 6,
+  S: 4,
+  T: 6,
+  U: 4,
+  V: 2,
+  W: 2,
+  X: 1,
+  Y: 2,
+  Z: 1,
+};
+
 export const drawLetters = () => {
-  // Implement this method for wave 1
+  //copy of LETTER_POOL so that original object doesn't change
+  let letterPool = {
+    ...LETTER_POOL,
+  };
+  console.log(letterPool)
+  
+  // looping through the letters/key and then adding corresponding
+  // #in value to array
+  let availableLetters = [];
+  for (let letter in letterPool) {
+    for (let i = 0; i < letterPool[letter]; i++) {
+      availableLetters.push(letter);
+    }
+  }
+  console.log(availableLetters)
+  // creating a hand with 10 random letters
+  let handArray = [];
+  while (handArray.length < 10) {
+    let randomNumber = Math.floor(Math.random() * availableLetters.length)
+    let randomLetter = availableLetters[randomNumber];
+    // if (availableLetters[hand] >= 1) {
+    //   handArray.push(hand);
+    //   availableLetters[hand] -= 1;
+    // }
+    console.log(`this is the random letter ${randomLetter}`)
+    console.log(`this is the random number ${randomNumber}`)
+    handArray.push(randomLetter);
+    availableLetters.splice(randomNumber, 1)
+  }
+  console.log(handArray)
+  return handArray
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
