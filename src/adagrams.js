aaa41f907c79f3faa74876c5063ff6b54a318791
Kate <varindakate@gmail.com>
2022-12-04 15:27:27 -0800

    add codes inside function

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,68 +1,51 @@
-let letterDist = {
-  A: 9,
-  N: 6,
-  B: 2,
-  O: 8,
-  C: 2,
-  P: 2,
-  D: 4,
-  Q: 1,
-  E: 12,
-  R: 6,
-  F: 2,
-  S: 4,
-  G: 3,
-  T: 6,
-  H: 2,
-  U: 4,
-  I: 9,
-  V: 2,
-  J: 1,
-  W: 2,
-  K: 1,
-  X: 1,
-  L: 4,
-  Y: 2,
-  M: 2,
-  Z: 1,
-};
-
-//* create score dictionary
-let letterScore = {};
-//* create score dictionary
-const buildScore = (letters, score) => {
-  for (const letter of letters) {
-    letterScore[letter] = score;
-  }
-};
-//* create score dictionary
-buildScore(["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"], 1);
-buildScore(["D", "G"], 2);
-buildScore(["B", "C", "M", "P"], 3);
-buildScore(["F", "H", "V", "W", "Y"], 4);
-buildScore(["K"], 5);
-buildScore(["J", "X"], 8);
-buildScore(["Q", "Z"], 10);
-
-//calculate total numbers of letters in pool
-let totalLetters = 0;
-for (const value of Object.values(letterDist)) {
-  totalLetters += value;
-}
-//create letter pool from letter distribution
-let letterPool = [];
-for (const letter of Object.keys(letterDist)) {
-  for (let i in letterDist) {
-    letterPool.push(letter);
-  }
-}
-
 export const drawLetters = () => {
+  const letterDist = {
+    A: 9,
+    N: 6,
+    B: 2,
+    O: 8,
+    C: 2,
+    P: 2,
+    D: 4,
+    Q: 1,
+    E: 12,
+    R: 6,
+    F: 2,
+    S: 4,
+    G: 3,
+    T: 6,
+    H: 2,
+    U: 4,
+    I: 9,
+    V: 2,
+    J: 1,
+    W: 2,
+    K: 1,
+    X: 1,
+    L: 4,
+    Y: 2,
+    M: 2,
+    Z: 1,
+  };
+
+  let letterPool = [];
+  //create letter pool from letter distribution
+  for (const letter of Object.keys(letterDist)) {
+    for (let i = 0; (i = letterDist[letter]); i++) {
+      letterPool.push(letter);
+    }
+  }
+  //calculate total numbers of letters in pool
+  let totalLetters = 0;
+  for (const value of Object.values(letterDist)) {
+    totalLetters += value;
+  }
+
+  //draw letters from pool into hand
   let hand = [];
   for (let i = 1; (i = 10); i++) {
-    let drawnLetter = Math.random() * totalLetters;
-    delete letterPool[drawnLetter];
-    hand.push(letterPool[drawnLetter]);
+    let drawnLetter = letterPool[Math.random() * totalLetters];
+    hand.push(hand[drawnLetter]);
   }
 };
 
@@ -79,6 +62,23 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 };
 
 export const scoreWord = (word) => {
+  // create dictionary with score for each letter
+  let letterScore = {};
+  const BuildScoreDict = (letters, score) => {
+    for (const letter of letters) {
+      letterScore[letter] = score;
+    }
+  };
+
+  BuildScoreDict(["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"], 1);
+  BuildScoreDict(["D", "G"], 2);
+  BuildScoreDict(["B", "C", "M", "P"], 3);
+  BuildScoreDict(["F", "H", "V", "W", "Y"], 4);
+  BuildScoreDict(["K"], 5);
+  BuildScoreDict(["J", "X"], 8);
+  BuildScoreDict(["Q", "Z"], 10);
+
+  // calculate score
   score = 0;
   for (let letter of word) {
     score += letterScore[letter];
