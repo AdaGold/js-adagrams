55f5e80736233fadc57f4c2bd849e546ba2c890e
TombAnneX <109308405+AKTuminello@users.noreply.github.com>
2023-05-31 16:27:14 -0600

    Wave I commit, some tests working

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,4 +1,49 @@
 export const drawLetters = () => {
+  const letterPool = {
+    'A': 9,
+    'B': 2,
+    'C': 2,
+    'D': 4,
+    'E': 12,
+    'F': 2,
+    'G': 3,
+    'H': 2,
+    'I': 9,
+    'J': 1,
+    'K': 1,
+    'L': 4,
+    'M': 2,
+    'N': 6,
+    'O': 8,
+    'P': 2,
+    'Q': 1,
+    'R': 6,
+    'S': 4,
+    'T': 6,
+    'U': 4,
+    'V': 2,
+    'W': 2,
+    'X': 1,
+    'Y': 2,
+    'Z': 1
+  };
+
+  let hand = [];
+
+  for (let i = 0; i < 10; i++) {
+    let letter = Object.keys(letterPool)[Math.floor(Math.random() * 26)];
+    if (letterPool[letter] > 0) {
+      hand.push(letter);
+      letterPool[letter] -= 1;
+    } else {
+      i -= 1;
+    }
+  }
+
+  return hand;
+
+
+    
   // Implement this method for wave 1
 };
 
