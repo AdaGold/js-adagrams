a6354e630551aefb98b8969b0f6ea6811678d4b4
Jennifer Tam <jenniferwtam@gmail.com>
2023-06-04 20:40:16 -0400

    Wave 3 scoreWord passed 3 tests

--- src/adagrams.js
+++ src/adagrams.js
@@ -26,6 +26,7 @@ X : 1,
 Y : 2,
 Z : 1
 };
+
 export function drawLetters() {
   
   let hand = [];
@@ -74,7 +75,29 @@ export const usesAvailableLetters = (input, lettersInHand) => {
   };
 
 export const scoreWord = (word) => {
-  // Implement this method for wave 3
+  // Define the points for each letter
+  const letterPoints = {
+    A: 1, B: 3, C: 3, D: 2, E: 1, F: 4, G: 2, H: 4, I: 1, J: 8, K: 5,
+    L: 1, M: 3, N: 1, O: 1, P: 3, Q: 10, R: 1, S: 1, T: 1, U: 1, V: 4,
+    W: 4, X: 8, Y: 4, Z: 10
+  };
+
+  // Calculate the total score for the word
+  let score = 0;
+  for (let i = 0; i < word.length; i++) {
+    const letter = word[i].toUpperCase();
+    if (letterPoints[letter]) {
+      score += letterPoints[letter];
+    }
+  }
+
+  // Add 8 points if the word has 7, 8, 9, or 10 letters
+  if (word.length >= 7 && word.length <= 10) {
+    score += 8;
+  }
+  
+  // Return the total score
+  return score;
 };
 
 export const highestScoreFrom = (words) => {
