7306187c5c868944348487ccc8aef39796c22b2a
Abby <argoodm@gmail.com>
2023-06-06 23:12:17 -0400

    wave one passsing

--- src/adagrams.js
+++ src/adagrams.js
@@ -50,18 +50,16 @@ export const drawLetters = () => {
 
     
 
-    let chosenLetter = randomIndex(letterList);{
+    let chosenLetter = randomIndex(letterList);
       if (selectedLetters.reduce((count, letter) => count + (letter === chosenLetter ? 1 : 0), 0) < LETTER_POOL[chosenLetter]){
         selectedLetters.push(chosenLetter);}
       // console.log(selectedLetters);}
       else {
         continue;
-      }}
-
-
+      }
+  
+    }
   return selectedLetters;
-    };
-
 
 };
 
@@ -77,6 +75,7 @@ export const usesAvailableLetters = (input, lettersInHand) => {
     if (character in clone){
       clone.pop(character);
     }
+  
     else {
       return false;
     };
@@ -91,8 +90,8 @@ export const scoreWord = (word) => {
     const scores = {"A" : 1, "E": 1, "I": 1, "O": 1, "U": 1 ,"L": 1, "N": 1, "R": 1, "S": 1, "T": 1, "D": 2, "G": 2,"B": 3, "C": 3, "M": 3, "P": 3, "F": 4, "H": 4, "V": 4, "W": 4, "Y": 4,"K": 5, "J": 8, "X":8 ,"Q": 10, "Z": 10};
 
     for (let letter of word.toUpperCase()){
-        // word_score += scores[letter]
-        console.log(scores[letter])
+        wordScore += scores[letter]
+        console.log(`score:${scores[letter]}`)
     if (word.length >= 7){
         wordScore += 8};
 
