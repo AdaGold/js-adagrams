b50a2f4ef3d1967fe58210d002300706aa4cf993
Kristina N <87397396+kstinanguyen@users.noreply.github.com>
2024-11-22 14:23:55 -0800

    wave 3 and 4 implemented with all tests passing

--- src/adagrams.js
+++ src/adagrams.js
@@ -38,9 +38,53 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 };
 
 export const scoreWord = (word) => {
-  // Implement this method for wave 3
+  const scoreChart = {
+    'A': 1, 'B': 3, 'C': 3, 'D': 2, 'E': 1, 'F': 4, 'G': 2, 'H': 4, 'I': 1, 'J': 8, 'K': 5, 'L': 1, 'M': 3, 'N': 1, 'O': 1, 'P': 3, 'Q': 10, 'R': 1, 'S': 1, 'T': 1, 'U': 1, 'V': 4, 'W': 4, 'X': 8, 'Y': 4, 'Z': 10
+  };
+
+  const bonusPoints = 8;
+  let points = 0;
+  let nonAlphaChars = 0;
+
+  if (!word) {
+    return points;
+  };
+
+  for (let char of word.toUpperCase()) {
+    if (scoreChart[char]) {
+      points += scoreChart[char];
+    } else {
+      nonAlphaChars += 1;
+    }
+  };
+
+  if ((word.length - nonAlphaChars) >= 7 && (word.length - nonAlphaChars) <= 10) {
+    points += bonusPoints;
+  } 
+  return points;
 };
 
 export const highestScoreFrom = (words) => {
-  // Implement this method for wave 4
+  let highestScoringWord = words[0];
+  let highestScore = scoreWord(words[0]);
+
+  for (let i = 1; i < words.length; i++) {
+    let word = words[i]
+    let currentScore = scoreWord(word);
+
+    if (currentScore > highestScore) {
+      highestScore = currentScore;
+      highestScoringWord = word;
+    } else if (currentScore === highestScore) {
+      if (highestScoringWord.length === 10) {
+        continue;
+      } else if (word.length == 10 || word.length < highestScoringWord.length) {
+        highestScoringWord = word;
+      }
+    }
+  }
+  return {
+    word: highestScoringWord,
+    score: highestScore,
+  };
 };
